65bd4b5f60cb42ae142b72a79f3ad12b20cd4a79
Sefa <sefa@outlook.ie>
2024-08-05 00:39:40 +0300

    beautify

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-//https://github.com/alibuyuktatli
+// https://github.com/alibuyuktatli
 
 let ajaxLoading = false;
 
@@ -9,37 +9,31 @@ const GAME_STATE_PLAYING = 3
 const GAME_STATE_FINISHED = 4
 
 const getGameState = (state) => {
-    switch (state) {
-        case GAME_STATE_STARTUP:
-            return "Yükleniyor";
-        case GAME_STATE_PREGAME:
-            return "Lobi";
-        case GAME_STATE_SETTING_UP:
-            return "Başlıyor";
-        case GAME_STATE_PLAYING:
-            return "Devam ediyor";
-        case GAME_STATE_FINISHED:
-            return "Bitti";
-        default:
-            return "";
-    }
+	switch (state) {
+		case GAME_STATE_STARTUP:
+			return "Lobi";
+		case GAME_STATE_PREGAME:
+			return "Lobi";
+		case GAME_STATE_SETTING_UP:
+			return "Başlıyor";
+		case GAME_STATE_PLAYING:
+			return "Devam ediyor";
+		case GAME_STATE_FINISHED:
+			return "Bitti";
+		default:
+			return "";
+	}
 }
 
 $(document).ready(function () {
-    refresh()
-    setInterval(refresh, 30000);
-
-    $("#discord").on("click", function () {
-        window.open("https://discord.gg/ErFmSgzMkt")
-    })
-
-    $("#wiki").on("click", function () {
-        location.href = "https://wiki.turkb.us"
-    })
+	refresh();
+	setInterval(refresh, 30000);
 });
 
 const serverTemplate = (active, name, map, userCount, roundId, roundTime, roundState, ip) => {
-    return `<div class="server-panel">
+	const roundDuration = new Date(roundTime * 1000).toISOString().substring(11, 19);
+
+	return `<div class="server-panel">
     <div class="server-info">
         <div class="server-info__contents">
             <div class="status">
@@ -56,16 +50,16 @@ const serverTemplate = (active, name, map, userCount, roundId, roundTime, roundS
                 <div class="list-name">${userCount}</div>
             </div>
             <div class="server-info-backend">
-                <div class="label">Tur ID:</div>
+                <div class="label">Round ID:</div>
                 <div class="list-name">${roundId}</div>
             </div>
             <div class="server-info-backend">
-            <div class="label">Tur durumu:</div>
+            <div class="label">Round durumu:</div>
             <div class="list-name">${getGameState(roundState)}</div>
         </div>
             <div class="server-info-backend">
                 <div class="label">Round süresi:</div>
-                <div class="list-name">${new Date(roundTime * 1000).toISOString().substring(11, 19)}</div>
+                <div class="list-name">${roundDuration}</div>
             </div>
             <div class="server-info__filtertags">
                 <a href="byond://${ip}" class="filtertag">Bağlan</a>
@@ -76,30 +70,35 @@ const serverTemplate = (active, name, map, userCount, roundId, roundTime, roundS
 </div>`
 }
 
-const refresh = () => {
-    if (ajaxLoading)
-        return;
+function refresh() {
+	if (ajaxLoading)
+		return;
 
-    ajaxLoading = true
-    $.ajax({
-        type: 'GET',
-        url: 'https://api.turkb.us/v2/server',
-        dataType: 'json',
-        success: function (data) {
-            $(".server-list__container").empty()
-            if (data.length === 0) {
-                $(".server-list__container").append(`<p class="text-center lead">Aktif bir sunucu bulunamadı.</p>`)
-            } else {
-                data.forEach(server => {
-                    $(".server-list__container").append(serverTemplate(server.server_status, server.name, server.map, server.players, server.round_id, server.round_duration, server.gamestate, server.connection_info))
-                });
-            }
-            $("#alert-box").slideUp()
-            ajaxLoading = false
-        },
-        error: function (jqXHR, status, error) {
-            $("#alert-box").slideDown().text("API sunucusuna bağlanılamıyor.")
-            ajaxLoading = false
-        }
-    });
+	ajaxLoading = true;
+
+	const success = function (data) {
+		$(".server-list__container").empty();
+		if (data.length === 0) {
+			$(".server-list__container").append(`<p class="text-center lead">Aktif bir sunucu bulunamadı.</p>`);
+		} else {
+			data.forEach(server => {
+				$(".server-list__container").append(serverTemplate(server.server_status, server.name, server.map, server.players, server.round_id, server.round_duration, server.gamestate, server.connection_info));
+			});
+		}
+		$("#alert-box").slideUp();
+		ajaxLoading = false;
+	}
+
+	const error = function (jqXHR, status, error) {
+		$("#alert-box").slideDown().text("API sunucusuna bağlanılamıyor.");
+		ajaxLoading = false;
+	}
+
+	$.ajax({
+		type: 'GET',
+		url: 'https://api.turkb.us/v2/server',
+		dataType: 'json',
+		success,
+		error,
+	});
 }
